378781c516098b3ca3a631359a10dba7176b2295
Abhigyan Srivastava <captainabhigyan.clasher@gmail.com>
2022-07-20 13:50:21 +0530

    Code

--- Conditionals&Loops/Patterns/Pattern3.cpp
+++ Conditionals&Loops/Patterns/Pattern3.cpp
@@ -6,7 +6,22 @@ int main(){
     int n;
     cin>>n;
 
-    
+    int i=1;
+
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout << j << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
 
     return 0;
 }
+
+/* 1234 
+   1234
+   1234
+   1234 */
