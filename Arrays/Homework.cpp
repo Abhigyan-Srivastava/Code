83b2a56a3e451fda8f3dcca42ae66244a7fdeed3
Abhigyan Srivastava <captainabhigyan.clasher@gmail.com>
2022-09-06 11:36:10 +0530

    Adding Homework

--- Arrays/Homework.cpp
+++ Arrays/Homework.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main(){
+    
+    int size = 5;
+    int arr[size];
+
+    int val = 5;
+
+    for(int i=0;i<size;i++){
+        arr[i] = val;
+    }
+
+    cout << "The array is->";
+    for(int i=0;i<size;i++){
+        printf("%d",arr[i]);
+    }
 
     return 0;
 }
